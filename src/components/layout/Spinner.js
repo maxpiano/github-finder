459b16888898466227175aa293858f027e299ffe
Max B <maxbilov@gmail.com>
2020-05-20 00:10:27 +0200

    just refactoring

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,12 +1,20 @@
+// vim: ft=javascriptreact
+
 import React, { Fragment } from 'react';
 import spinner from './spinner.gif';
 
-const Spinner = () => {
-  return (
-    <Fragment>
-      <p>Body</p>
-    </Fragment>
-  );
-};
+const Spinner = () => (
+  <Fragment>
+    <img
+      src={spinner}
+      alt="Loading..."
+      style={{
+        width: '200px',
+        margin: 'autio',
+        display: 'block',
+      }}
+    />
+  </Fragment>
+);
 
 export default Spinner;
