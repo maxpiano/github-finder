c0ce7aa4cb98cd930f9b0445f7cdb482d6e17abd
Max B <maxbilov@gmail.com>
2020-05-19 19:04:54 +0200

    work on UserItem

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,9 +1,37 @@
 // vim: ft=javascriptreact
 
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class UserItem extends Component {
+  state = {
+    id: 1,
+    login: 'mojombo',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+    html_url: 'https://github.com/mojombo',
+  };
+
   render() {
-    return <div>UserItem</div>;
+    const { avatar_url, login, html_url } = this.state;
+    return (
+      <div className="card text-center">
+        <img
+          src={avatar_url}
+          alt="profile"
+          className="round-img"
+          style={{ width: '60px' }}
+        />
+        <h1>{login}</h1>
+        <div>
+          <a
+            href={html_url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-dark btn-sm my-1"
+          >
+            More
+          </a>
+        </div>
+      </div>
+    );
   }
 }
