b8c1e2e1cd700365e8e65247ef61a9d592d38b14
Max B <maxbilov@gmail.com>
2020-05-19 19:05:38 +0200

    added Users component

---
+++ src/components/users/Users.js
@@ -0,0 +1,15 @@
+// vim: ft=javascriptreact
+
+import React, { Component } from 'react';
+
+export default class Users extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return null;
+  }
+}
