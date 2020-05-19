7e823f57f404e370e704a369e968da991918245c
Max B <maxbilov@gmail.com>
2020-05-20 00:05:38 +0200

    add spinner gif and component

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import spinner from './spinner.gif';
+
+const Spinner = () => {
+  return (
+    <Fragment>
+      <p>Body</p>
+    </Fragment>
+  );
+};
+
+export default Spinner;
