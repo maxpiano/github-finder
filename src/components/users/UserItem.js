499c6314b703aae656c4c064517aef90e4873993
Max B <maxbilov@gmail.com>
2020-05-19 19:31:12 +0200

    removed state from  useritem

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -3,13 +3,6 @@
 import React, { Component } from 'react';
 
 export default class UserItem extends Component {
-  state = {
-    id: 1,
-    login: 'mojombo',
-    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-    html_url: 'https://github.com/mojombo',
-  };
-
   render() {
     const { avatar_url, login, html_url } = this.props.user;
     return (
