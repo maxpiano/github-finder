0d304ae8b43fb18ae86f775c7c01741e711c339f
Max B <maxbilov@gmail.com>
2020-05-19 20:01:27 +0200

    added style to users container

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -29,7 +29,7 @@ export default class Users extends Component {
 
   render() {
     return (
-      <div>
+      <div style={userStyle}>
         {this.state.users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
@@ -37,3 +37,9 @@ export default class Users extends Component {
     );
   }
 }
+
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem',
+};
