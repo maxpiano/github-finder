71b607247f30ecfc8938cb5aa37593a71e90344d
Max B <maxbilov@gmail.com>
2020-05-19 18:52:52 +0200

    navbar and useritem

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,25 @@
 // vim: ft=javascriptreact
 
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class Navbar extends Component {
+  static defaultProps = {
+    title: "Github Finder",
+    icon: "fab fa-github",
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired(),
+    icon: PropTypes.string.isRequired(),
+  };
+
   render() {
     return (
       <nav className="navbar bg-primary">
-        <h1>navbar</h1>
+        <h1>
+          <div className={this.props.icon}></div> {this.props.title}
+        </h1>
       </nav>
     );
   }
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,9 @@
+// vim: ft=javascriptreact
+
+import React, { Component } from "react";
+
+export default class UserItem extends Component {
+  render() {
+    return <div>UserItem</div>;
+  }
+}
