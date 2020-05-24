74d341c7c8a5d936277cc36aa09aae0971c56962
Max B <maxbilov@gmail.com>
2020-05-25 00:07:55 +0200

    added propTypes to search component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,9 +1,14 @@
 // vim: ft=javascriptreact
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Search extends Component {
   state = {
     text: '',
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
   };
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
