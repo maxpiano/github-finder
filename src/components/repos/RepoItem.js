c33b72f615d88f678a933b0c044cdc1503be699b
Max B <maxbilov@gmail.com>
2020-05-25 19:14:48 +0200

    successfully implemented repos and repoitem

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -3,7 +3,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const RepoItem = ({ repo }) => {
-  return null;
+  return (
+    <div className="card">
+      <h3>
+        <a href={repo.html_url}>{repo.name}</a>
+      </h3>
+    </div>
+  );
 };
 
 RepoItem.propTypes = {
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,6 +1,7 @@
 // vim: ft=javascriptreact
 import React, { Component, Fragment } from 'react';
 import Spinner from '../layout/Spinner';
+import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
@@ -14,6 +15,7 @@ export default class User extends Component {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
     getUser: PropTypes.func.isRequired,
+    repos: PropTypes.array.isRequired,
     getUserRepos: PropTypes.func.isRequired,
   };
 
@@ -33,7 +35,7 @@ export default class User extends Component {
       hireable,
       company,
     } = this.props.user;
-    const { loading } = this.props;
+    const { repos, loading } = this.props;
 
     if (loading) return <Spinner />;
     return (
@@ -99,6 +101,7 @@ export default class User extends Component {
           <div className="badge badge-light">Public Repos: {public_repos}</div>
           <div className="badge badge-dark">Public Gists: {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
