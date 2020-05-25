7a38d6a0d703392dc18f2ae084f4f1ca11936a99
Max B <maxbilov@gmail.com>
2020-05-25 17:49:52 +0200

    added name, location and bio

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -52,6 +52,16 @@ export default class User extends Component {
               className="round-img"
               style={{ width: '150px' }}
             />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
           </div>
         </div>
       </Fragment>
