38e486a0eff1e74ebba25712eec94d6ae3fb5037
Max B <maxbilov@gmail.com>
2020-05-25 16:50:28 +0200

    get user data into user component via state and props

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
     const res = await axios.get(
       `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({ users: res.data, loading: false });
+    this.setState({ user: res.data, loading: false });
   };
 
   clearUsers = () => this.setState({ users: [], loading: false });
