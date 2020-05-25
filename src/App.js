92f85212976dd517648f1e716845f324cd3bad8a
Max B <maxbilov@gmail.com>
2020-05-25 18:35:58 +0200

    add user repos method to app

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends Component {
     user: {},
     loading: false,
     alert: null,
+    repos: [],
   };
 
   searchUsers = async (text) => {
@@ -34,6 +35,14 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:acs&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false });
+  };
+
   clearUsers = () => this.setState({ users: [], loading: false });
 
   setAlert = (msg, type) => {
@@ -48,7 +57,6 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar />
-
           <div className="container">
             <Alert alert={this.state.alert} />
             <Switch>
