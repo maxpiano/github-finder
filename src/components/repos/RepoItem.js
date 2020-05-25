eded9d9238d24f9f6d2002622873e065cdbda83a
Max B <maxbilov@gmail.com>
2020-05-25 19:02:44 +0200

    added repos.js and repoitem.js

---
+++ src/components/repos/RepoItem.js
@@ -0,0 +1,13 @@
+// vim: ft=javascriptreact
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const RepoItem = ({ repo }) => {
+  return null;
+};
+
+RepoItem.propTypes = {
+  repo: PropTypes.object.isRequired,
+};
+
+export default RepoItem;
