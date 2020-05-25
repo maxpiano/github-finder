1d193945bae8b4dbd0507f32359e35d0270d721f
Max B <maxbilov@gmail.com>
2020-05-25 18:13:42 +0200

    added username, company and website

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -29,6 +29,7 @@ export default class User extends Component {
       public_repos,
       public_gists,
       hireable,
+      company,
     } = this.props.user;
     const { loading } = this.props;
 
@@ -62,7 +63,36 @@ export default class User extends Component {
                 <p>{bio}</p>
               </Fragment>
             )}
+            <a href={html_url} className="btn btn-dark my-1">
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username:</strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company:</strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website:</strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
           </div>
+        </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers: {followers}</div>
         </div>
       </Fragment>
     );
